e8f3eaca4e490534ab15219519c9a58de08f8c75
o-ga <54522966+o-ga09@users.noreply.github.com>
2023-06-08 23:05:38 +0900

    fix (#19)

--- api/service/monster_test.go
+++ api/service/monster_test.go
@@ -7,6 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var weakness = map[string]string{
+	"頭部": "10",
+	"前脚": "10",
+	"胴体": "10",
+	"後脚": "10",
+	"尻尾": "10",
+}
+
 func Test_GetAll(t *testing.T) {
 	monsterInterface := new(MockMonsterInterface)
 	service := MonsterService{monsterInterface}
@@ -28,7 +36,7 @@ func Test_GetById(t *testing.T) {
 func Test_Create(t *testing.T) {
 	monsterInterface := new(MockMonsterInterface)
 	service := MonsterService{monsterInterface}
-	monsterJson := entity.MonsterJson{Name: entity.MonsterName{Value: "ジンオウガ"},Desc: entity.MonsterDesc{Value: "渓流に生息する牙竜種"},Location: entity.MonsterLocation{Value: "渓流"},Specify: entity.MonsterSpecify{Value: "牙竜種"},Weakness_attack: entity.MonsterWeakness_A{Value: "頭部"},Weakness_element: entity.MonsterWeakness_E{Value: "氷"}}
+	monsterJson := entity.MonsterJson{Name: entity.MonsterName{Value: "ジンオウガ"},Desc: entity.MonsterDesc{Value: "渓流に生息する牙竜種"},Location: entity.MonsterLocation{Value: "渓流"},Specify: entity.MonsterSpecify{Value: "牙竜種"},Weakness_attack: entity.MonsterWeakness_A{Value: weakness},Weakness_element: entity.MonsterWeakness_E{Value: weakness}}
 	monsterInterface.On("Create",monsterJson).Return(nil)
 	actual := service.Create(monsterJson)
 	assert.Equal(t,nil,actual)
@@ -38,7 +46,7 @@ func Test_Update(t *testing.T) {
 	mockInterface := new(MockMonsterInterface)
 	service := MonsterService{mockInterface}
 	id := entity.MonsterId{Value: 1}
-	monsterJson := entity.MonsterJson{Name: entity.MonsterName{Value: "ジンオウガ"},Desc: entity.MonsterDesc{Value: "渓流に生息する牙竜種"},Location: entity.MonsterLocation{Value: "渓流"},Specify: entity.MonsterSpecify{Value: "牙竜種"},Weakness_attack: entity.MonsterWeakness_A{Value: "頭部"},Weakness_element: entity.MonsterWeakness_E{Value: "氷"}}
+	monsterJson := entity.MonsterJson{Name: entity.MonsterName{Value: "ジンオウガ"},Desc: entity.MonsterDesc{Value: "渓流に生息する牙竜種"},Location: entity.MonsterLocation{Value: "渓流"},Specify: entity.MonsterSpecify{Value: "牙竜種"},Weakness_attack: entity.MonsterWeakness_A{Value: weakness},Weakness_element: entity.MonsterWeakness_E{Value: weakness}}
 	mockInterface.On("Update",id,monsterJson).Return(nil)
 	err := service.Update(id,monsterJson)
 	assert.Equal(t,nil,err)
