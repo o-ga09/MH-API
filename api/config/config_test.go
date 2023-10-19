cba64beac47a7a79bd616f774f008ca616720d5b
taiti09 <taitiabe1997@gmail.com>
2023-10-20 00:07:40 +0900

    fix : fmt

--- api/config/config_test.go
+++ api/config/config_test.go
@@ -11,14 +11,14 @@ func TestNew(t *testing.T) {
 		want    *Config
 		wantErr bool
 	}{
-		{name: "正常系", want: &Config{Env: "dev", Port: "8080", Database_url: "user:password@tcp(localhost)/test?charset=utf8&parseTime=True&loc=Local&tls=true", ProjectID: "00000",ALLOW_URL: "*"}, wantErr: false},
+		{name: "正常系", want: &Config{Env: "dev", Port: "8080", Database_url: "user:password@tcp(localhost)/test?charset=utf8&parseTime=True&loc=Local&tls=true", ProjectID: "00000", ALLOW_URL: "*"}, wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Setenv("ENV", tt.want.Env)
 		t.Setenv("PORT", tt.want.Port)
 		t.Setenv("DATABASE_URL", tt.want.Database_url)
 		t.Setenv("PROJECTID", tt.want.ProjectID)
-		t.Setenv("ALLOW_URL",tt.want.ALLOW_URL)
+		t.Setenv("ALLOW_URL", tt.want.ALLOW_URL)
 
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := New()
