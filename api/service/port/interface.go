8cae983ffd53b7a7ed1fc95f22177965396f9648
taiti09 <taitiabe1997@gmail.com>
2023-11-11 00:39:08 +0900

    fix : テーブル定義修正 #53

---
+++ api/service/port/interface.go
@@ -0,0 +1,11 @@
+package port
+
+import "mh-api/api/entity"
+
+type MonsterInterface interface {
+	GetAll() (entity.Monsters, error)
+	GetById(id entity.MonsterId) (entity.Monster, error)
+	Create(monsterJson entity.MonsterJson) error
+	Update(id entity.MonsterId, monsterJson entity.MonsterJson) error
+	Delete(id entity.MonsterId) error
+}
