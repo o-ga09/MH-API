1ef5f7ca21d449d8e3edb5c8ecaead57fe8048a5
taiti09 <taitiabe1997@gmail.com>
2024-04-12 08:41:39 +0900

    Monster Repositoryのテスト完了

--- app/internal/driver/mysql/monsters_test.go
+++ app/internal/driver/mysql/monsters_test.go
@@ -3,6 +3,7 @@ package mysql
 import (
 	"context"
 	"mh-api/app/internal/domain/monsters"
+	"os"
 	"reflect"
 	"testing"
 
@@ -10,6 +11,9 @@ import (
 )
 
 func TestNewMonsterRepository(t *testing.T) {
+	os.Setenv("DATABASE_URL", "mh-api:P@ssw0rd@tcp(127.0.0.1:3306)/mh-api?charset=utf8&parseTime=True&loc=Local")
+	conn := New(context.Background())
+
 	type args struct {
 		conn *gorm.DB
 	}
@@ -18,7 +22,7 @@ func TestNewMonsterRepository(t *testing.T) {
 		args args
 		want *monsterRepository
 	}{
-		// TODO: Add test cases.
+		{name: "MonsetrRepository構造体を生成する", args: args{conn: conn}, want: NewMonsterRepository(conn)},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -30,11 +34,22 @@ func TestNewMonsterRepository(t *testing.T) {
 }
 
 func Test_monsterRepository_Get(t *testing.T) {
+	BeforeTest()
+	t.Cleanup(AfetrTest())
+	conn := New(context.Background())
+
+	wantMonster1 := monsters.NewMonster("0000000001", "リオレウス", "空の王者。")
+	wantMonster2 := monsters.NewMonster("0000000002", "リオレイア", "陸の女王")
+	wantMonster3 := monsters.NewMonster("0000000003", "ティガレックス", "絶対強者")
+
+	wantMonsters1 := monsters.Monsters{
+		wantMonster1, wantMonster2, wantMonster3,
+	}
+
 	type fields struct {
 		conn *gorm.DB
 	}
 	type args struct {
-		ctx       context.Context
 		monsterId string
 	}
 	tests := []struct {
@@ -44,14 +59,15 @@ func Test_monsterRepository_Get(t *testing.T) {
 		want    monsters.Monsters
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{name: "DBからモンスターデータを複数件取得できる", fields: fields{conn: conn}, args: args{monsterId: ""}, want: wantMonsters1, wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			r := &monsterRepository{
 				conn: tt.fields.conn,
 			}
-			got, err := r.Get(tt.args.ctx, tt.args.monsterId)
+			ctx := context.Background()
+			got, err := r.Get(ctx, tt.args.monsterId)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("monsterRepository.Get() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -64,12 +80,17 @@ func Test_monsterRepository_Get(t *testing.T) {
 }
 
 func Test_monsterRepository_Save(t *testing.T) {
+	BeforeTest()
+	t.Cleanup(AfetrTest())
+	conn := New(context.Background())
+
+	saveMonster1 := monsters.NewMonster("0000000004", "ライゼクス", "雷の反逆者")
+
 	type fields struct {
 		conn *gorm.DB
 	}
 	type args struct {
-		ctx context.Context
-		m   monsters.Monster
+		m monsters.Monster
 	}
 	tests := []struct {
 		name    string
@@ -77,14 +98,15 @@ func Test_monsterRepository_Save(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{name: "DBからモンスターデータを複数件取得できる", fields: fields{conn: conn}, args: args{m: saveMonster1}, wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			r := &monsterRepository{
 				conn: tt.fields.conn,
 			}
-			if err := r.Save(tt.args.ctx, tt.args.m); (err != nil) != tt.wantErr {
+			ctx := context.Background()
+			if err := r.Save(ctx, tt.args.m); (err != nil) != tt.wantErr {
 				t.Errorf("monsterRepository.Save() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
@@ -92,11 +114,14 @@ func Test_monsterRepository_Save(t *testing.T) {
 }
 
 func Test_monsterRepository_Remove(t *testing.T) {
+	BeforeTest()
+	t.Cleanup(AfetrTest())
+	conn := New(context.Background())
+
 	type fields struct {
 		conn *gorm.DB
 	}
 	type args struct {
-		ctx       context.Context
 		monsterId string
 	}
 	tests := []struct {
@@ -105,14 +130,15 @@ func Test_monsterRepository_Remove(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{name: "DBからモンスターデータを複数件取得できる", fields: fields{conn: conn}, args: args{monsterId: "0000000001"}, wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			r := &monsterRepository{
 				conn: tt.fields.conn,
 			}
-			if err := r.Remove(tt.args.ctx, tt.args.monsterId); (err != nil) != tt.wantErr {
+			ctx := context.Background()
+			if err := r.Remove(ctx, tt.args.monsterId); (err != nil) != tt.wantErr {
 				t.Errorf("monsterRepository.Remove() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
