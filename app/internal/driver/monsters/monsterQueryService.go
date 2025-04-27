24598236a5adb296e955c6ae1fa7b8462c2d59de
o-ga09 <taitiabe1997@gmail.com>
2025-04-27 09:44:01 +0900

    ✨ feat: FetchListメソッドにランキング情報を追加し、FetchRankメソッドのテストを削除しました

--- app/internal/driver/monsters/monsterQueryService.go
+++ app/internal/driver/monsters/monsterQueryService.go
@@ -77,6 +77,7 @@ func (s *monsterQueryService) FetchList(ctx context.Context, id string) ([]*mons
 		var weak_attack []monsters.Weakness_attack
 		var weak_element []monsters.Weakness_element
 		var firstWeaknessAttack, secondWeaknessAttack, firstWeaknesselement, secondWeaknessElement string
+		var ranking []monsters.Ranking
 
 		for _, f := range m.Field {
 			l = append(l, f.Name)
@@ -103,6 +104,13 @@ func (s *monsterQueryService) FetchList(ctx context.Context, id string) ([]*mons
 			})
 		}
 
+		for _, r := range m.Ranking {
+			ranking = append(ranking, monsters.Ranking{
+				Ranking:  r.Ranking,
+				VoteYear: r.VoteYear,
+			})
+		}
+
 		if len(m.Weakness) > 0 {
 			firstWeaknessAttack = m.Weakness[0].FirstWeakAttack
 			secondWeaknessAttack = m.Weakness[0].SecondWeakAttack
@@ -123,111 +131,9 @@ func (s *monsterQueryService) FetchList(ctx context.Context, id string) ([]*mons
 			SecondWeak_Element: secondWeaknessElement,
 			Weakness_attack:    weak_attack,
 			Weakness_element:   weak_element,
+			Ranking:            ranking,
 		}
 		res = append(res, &r)
-	}
-	return res, err
-}
-
-func (s *monsterQueryService) FetchRank(ctx context.Context) ([]*monsters.FetchMonsterRankingDto, error) {
-	var monster []mysql.Monster
-	var monsterIds []string
-	var result *gorm.DB
-	var p param.RequestRankingParam
-	var err error
-
-	where_clade := ""
-
-	where_clade_field := ""
-	where_clade_tribe := ""
-	where_clade_title := ""
-
-	sort := ""
-
-	p = ctx.Value("param").(param.RequestRankingParam)
-
-	limit := p.Limit
-	offset := p.Offset
-
-	if p.MonsterIds != "" {
-		monsterIds = strings.Split(p.MonsterIds, ",")
-		where_clade = "monster_id IN (?)"
-	}
-
-	if p.MonsterName != "" && where_clade != "" {
-		where_clade += " and name LIKE '%" + p.MonsterName + "%' "
-	} else if p.MonsterName != "" {
-		where_clade += " name LIKE '%" + p.MonsterName + "%' "
-	}
-
-	if p.LocationName != "" {
-		where_clade_field += " name LIKE '%" + p.LocationName + "%' "
-	}
-
-	if p.TribeName != "" {
-		where_clade_tribe += " name_ja LIKE '%" + p.TribeName + "%' "
-	}
-
-	if p.Title != "" {
-		where_clade_title += " name LIKE '%" + p.Title + "%' "
-	}
-
-	if p.Sort == "2" {
-		sort = "monster_id DESC"
-	} else {
-		sort = "monster_id ASC"
-	}
-
-	if where_clade != "" && p.MonsterIds != "" {
-		result = s.conn.Model(&monster).Preload("Field", where_clade_field).Preload("Tribe", where_clade_tribe).Preload("Product", where_clade_title).Preload("Ranking").Preload("Weakness").Where(where_clade, monsterIds).Limit(limit).Offset(offset).Order(sort).Find(&monster)
-	} else if where_clade != "" {
-		result = s.conn.Model(&monster).Preload("Field", where_clade_field).Preload("Tribe", where_clade_tribe).Preload("Product", where_clade_title).Preload("Ranking").Preload("Weakness").Where(where_clade).Limit(limit).Offset(offset).Order(sort).Find(&monster)
-	} else {
-		result = s.conn.Model(&monster).Preload("Ranking").Preload("Field", where_clade_field).Preload("Tribe", where_clade_tribe).Preload("Product", where_clade_title).Preload("Weakness").Limit(limit).Offset(offset).Order(sort).Find(&monster)
-	}
-
-	if result.Error != nil {
-		return nil, fmt.Errorf("error: %s", result.Statement.Error)
-	} else if result.RowsAffected == 0 {
-		return nil, gorm.ErrRecordNotFound
-	}
-	res := []*monsters.FetchMonsterRankingDto{}
-	for _, m := range monster {
-		if IsPreloadNotFound(&m) {
-			continue
-		}
-		var l []string
-		var t []string
-		var rank []monsters.Ranking
-
-		for _, f := range m.Field {
-			l = append(l, f.Name)
-		}
-		for _, p := range m.Product {
-			t = append(t, p.Name)
-		}
-
-		for _, r := range m.Ranking {
-			rank = append(rank, monsters.Ranking{
-				Ranking:  r.Ranking,
-				VoteYear: r.VoteYear,
-			})
-		}
-
-		r := monsters.FetchMonsterRankingDto{
-			Id:          m.MonsterId,
-			Name:        m.Name,
-			Description: m.Description,
-			Location:    l,
-			Category:    m.Tribe.Name_ja,
-			Title:       t,
-			Ranking:     rank,
-		}
-		res = append(res, &r)
-	}
-
-	if len(res) == 0 || res == nil {
-		return nil, gorm.ErrRecordNotFound
 	}
 	return res, err
 }
