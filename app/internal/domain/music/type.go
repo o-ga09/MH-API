cabb90af579a5ffde12d491710d72f1f60901df2
o-ga09 <taitiabe1997@gmail.com>
2025-04-27 13:40:36 +0900

    ✨ feat: Music構造体のimageUrlフィールドをUrlフィールドに変更し、関連する関数を更新しました

--- app/internal/domain/music/type.go
+++ app/internal/domain/music/type.go
@@ -4,9 +4,9 @@ type Musics []Music
 
 type MusicId struct{ value string }
 type MusicName struct{ value string }
-type MusicImageUrl struct{ value string }
+type MusicUrl struct{ value string }
 
 func (f *Music) GetMonsterID() string { return f.monsterId.Value }
 func (f *Music) GetID() string        { return f.musicId.value }
 func (f *Music) GetName() string      { return f.name.value }
-func (f *Music) GetURL() string       { return f.imageUrl.value }
+func (f *Music) GetURL() string       { return f.Url.value }
