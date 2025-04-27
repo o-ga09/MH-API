27ae86de77ded4bb50ddf5a5b75e20f9d8f81114
o-ga09 <taitiabe1997@gmail.com>
2025-04-27 13:59:44 +0900

    🔧 fix: GetItemsおよびGetItemByMonsterの未実装のドキュメントを更新しました

--- app/internal/controller/item/handler.go
+++ app/internal/controller/item/handler.go
@@ -16,18 +16,18 @@ func NewItemHandler(s monsters.MonsterService) *ItemHandler {
 	}
 }
 
-// GetItems godoc
-// @Summary アイテム検索（複数件）
-// @Description アイテムを検索して、条件に合致するアイテムを複数件取得する
-// @Tags アイテム検索
-// @Accept json
-// @Produce json
-// @Param request query RequestParam true  "クエリパラメータ"
-// @Success 200 {object} Items
-// @Failure      400  {object}  MessageResponse
-// @Failure      404  {object}  MessageResponse
-// @Failure      500  {object}  MessageResponse
-// @Router /items [get]
+// NOT IMPLEMENT GetItems godoc
+// NOT IMPLEMENT @Summary アイテム検索（複数件）
+// NOT IMPLEMENT @Description アイテムを検索して、条件に合致するアイテムを複数件取得する
+// NOT IMPLEMENT @Tags アイテム検索
+// NOT IMPLEMENT @Accept json
+// NOT IMPLEMENT @Produce json
+// NOT IMPLEMENT @Param request query RequestParam true  "クエリパラメータ"
+// NOT IMPLEMENT @Success 200 {object} Items
+// NOT IMPLEMENT @Failure      400  {object}  MessageResponse
+// NOT IMPLEMENT @Failure      404  {object}  MessageResponse
+// NOT IMPLEMENT @Failure      500  {object}  MessageResponse
+// NOT IMPLEMENT @Router /items [get]
 func (h *ItemHandler) GetItems(c *gin.Context) {}
 
 // GetItems godoc
@@ -44,16 +44,16 @@ func (h *ItemHandler) GetItems(c *gin.Context) {}
 // @Router /items/:itemId [get]
 func (h *ItemHandler) GetItem(c *gin.Context) {}
 
-// GetItems godoc
-// @Summary アイテム検索（モンスター別）
-// @Description 指定のアイテムが取得可能なモンスターの一覧
-// @Tags アイテム検索
-// @Accept json
-// @Produce json
-// @Param request query RequestParam true  "クエリパラメータ"
-// @Success 200 {object} ItemsByMonster
-// @Failure      400  {object}  MessageResponse
-// @Failure      404  {object}  MessageResponse
-// @Failure      500  {object}  MessageResponse
-// @Router /items/monsters [get]
+// NOT IMPLEMENT GetItems godoc
+// NOT IMPLEMENT @Summary アイテム検索（モンスター別）
+// NOT IMPLEMENT @Description 指定のアイテムが取得可能なモンスターの一覧
+// NOT IMPLEMENT @Tags アイテム検索
+// NOT IMPLEMENT @Accept json
+// NOT IMPLEMENT @Produce json
+// NOT IMPLEMENT @Param request query RequestParam true  "クエリパラメータ"
+// NOT IMPLEMENT @Success 200 {object} ItemsByMonster
+// NOT IMPLEMENT @Failure      400  {object}  MessageResponse
+// NOT IMPLEMENT @Failure      404  {object}  MessageResponse
+// NOT IMPLEMENT @Failure      500  {object}  MessageResponse
+// NOT IMPLEMENT @Router /items/monsters [get]
 func (h *ItemHandler) GetItemByMonster(c *gin.Context) {}
