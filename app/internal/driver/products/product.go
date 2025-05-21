fd42848458b6ff75ed98b91f4cbdf1812c13b66d
o-ga09 <taitiabe1997@gmail.com>
2025-05-21 23:45:26 +0900

    🔧 fix: gorm.DB接続を削除し、productRepositoryのテストを修正しました

--- app/internal/driver/products/product.go
+++ app/internal/driver/products/product.go
@@ -4,18 +4,12 @@ import (
 	"context"
 	Products "mh-api/app/internal/domain/products"
 	"mh-api/app/internal/driver/mysql"
-
-	"gorm.io/gorm"
 )
 
-type productRepository struct {
-	conn *gorm.DB
-}
+type productRepository struct{}
 
-func NewMonsterRepository(conn *gorm.DB) *productRepository {
-	return &productRepository{
-		conn: conn,
-	}
+func NewMonsterRepository() *productRepository {
+	return &productRepository{}
 }
 
 func (r *productRepository) Save(ctx context.Context, p Products.Product) error {
@@ -25,9 +19,9 @@ func (r *productRepository) Save(ctx context.Context, p Products.Product) error 
 		PublishYear: p.GetYear(),
 		TotalSales:  p.GetSales(),
 	}
-	r.conn.Exec("SET foreign_key_checks = 0")
-	err := r.conn.Save(&product).Error
-	r.conn.Exec("SET foreign_key_checks = 1")
+	mysql.CtxFromDB(ctx).Exec("SET foreign_key_checks = 0")
+	err := mysql.CtxFromDB(ctx).Save(&product).Error
+	mysql.CtxFromDB(ctx).Exec("SET foreign_key_checks = 1")
 	if err != nil {
 		return err
 	}
@@ -38,7 +32,7 @@ func (r *productRepository) Remove(ctx context.Context, productId string) error 
 	product := mysql.Product{
 		ProductId: productId,
 	}
-	err := r.conn.Delete(&product).Error
+	err := mysql.CtxFromDB(ctx).Delete(&product).Error
 	if err != nil {
 		return err
 	}
