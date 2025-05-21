75177f94d8692e66d7b59170ca6a3abb055422f0
o-ga09 <taitiabe1997@gmail.com>
2025-05-21 23:44:59 +0900

    🔧 fix: itemRepositoryからgorm.DB接続を削除し、テストを修正しました

--- app/internal/driver/items/item.go
+++ app/internal/driver/items/item.go
@@ -5,18 +5,12 @@ import (
 	"fmt"
 	"mh-api/app/internal/domain/items"
 	"mh-api/app/internal/driver/mysql"
-
-	"gorm.io/gorm"
 )
 
-type itemRepository struct {
-	conn *gorm.DB
-}
+type itemRepository struct{}
 
-func NewMonsterRepository(conn *gorm.DB) *itemRepository {
-	return &itemRepository{
-		conn: conn,
-	}
+func NewMonsterRepository() *itemRepository {
+	return &itemRepository{}
 }
 
 func (r *itemRepository) Save(ctx context.Context, m items.Item) error {
@@ -25,11 +19,11 @@ func (r *itemRepository) Save(ctx context.Context, m items.Item) error {
 		Name:     m.GetName(),
 		ImageUrl: m.GetURL(),
 	}
-	r.conn.Exec("SET foreign_key_checks = 0")
-	if err := r.conn.Save(&i).Error; err != nil {
+	mysql.CtxFromDB(ctx).Exec("SET foreign_key_checks = 0")
+	if err := mysql.CtxFromDB(ctx).Save(&i).Error; err != nil {
 		return fmt.Errorf("%s", err.Error())
 	}
-	r.conn.Exec("SET foreign_key_checks = 1")
+	mysql.CtxFromDB(ctx).Exec("SET foreign_key_checks = 1")
 	return nil
 }
 
@@ -37,7 +31,7 @@ func (r *itemRepository) Remove(ctx context.Context, itemId string) error {
 	item := mysql.Item{
 		ItemId: itemId,
 	}
-	err := r.conn.Delete(&item).Error
+	err := mysql.CtxFromDB(ctx).Delete(&item).Error
 	if err != nil {
 		return err
 	}
--- app/internal/driver/items/item_test.go
+++ app/internal/driver/items/item_test.go
@@ -6,27 +6,21 @@ import (
 	"mh-api/app/internal/driver/mysql"
 	"reflect"
 	"testing"
-
-	"gorm.io/gorm"
 )
 
 func TestNewMonsterRepository(t *testing.T) {
 	mysql.BeforeTest()
 	t.Cleanup(mysql.AfetrTest())
-	conn := mysql.New(context.Background())
-	type args struct {
-		conn *gorm.DB
-	}
+
 	tests := []struct {
 		name string
-		args args
 		want *itemRepository
 	}{
-		{name: "TestNewMonsterRepository", args: args{conn: conn}, want: &itemRepository{conn: conn}},
+		{name: "TestNewMonsterRepository", want: &itemRepository{}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewMonsterRepository(tt.args.conn); !reflect.DeepEqual(got, tt.want) {
+			if got := NewMonsterRepository(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewMonsterRepository() = %v, want %v", got, tt.want)
 			}
 		})
@@ -36,30 +30,24 @@ func TestNewMonsterRepository(t *testing.T) {
 func Test_itemRepository_Save(t *testing.T) {
 	mysql.BeforeTest()
 	t.Cleanup(mysql.AfetrTest())
-	conn := mysql.New(context.Background())
-	type fields struct {
-		conn *gorm.DB
-	}
+
 	type args struct {
 		ctx context.Context
 		m   items.Item
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		wantErr bool
 	}{
 		// test 1
-		{name: "Save item successfully", fields: fields{conn: conn}, args: args{ctx: context.Background(), m: *items.NewItem("0000000001", "test", "test")}, wantErr: false},
+		{name: "Save item successfully", args: args{ctx: context.Background(), m: *items.NewItem("0000000001", "test", "test")}, wantErr: false},
 		// test 2
-		{name: "Save item with error", fields: fields{conn: conn}, args: args{ctx: context.Background(), m: *items.NewItem("@$%&^#%$&&*%*&)(*)()", "", "")}, wantErr: true},
+		{name: "Save item with error", args: args{ctx: context.Background(), m: *items.NewItem("@$%&^#%$&&*%*&)(*)()", "", "")}, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := &itemRepository{
-				conn: tt.fields.conn,
-			}
+			r := &itemRepository{}
 			if err := r.Save(tt.args.ctx, tt.args.m); (err != nil) != tt.wantErr {
 				t.Errorf("itemRepository.Save() error = %v, wantErr %v", err, tt.wantErr)
 			}
@@ -70,30 +58,24 @@ func Test_itemRepository_Save(t *testing.T) {
 func Test_itemRepository_Remove(t *testing.T) {
 	mysql.BeforeTest()
 	t.Cleanup(mysql.AfetrTest())
-	conn := mysql.New(context.Background())
-	type fields struct {
-		conn *gorm.DB
-	}
+
 	type args struct {
 		ctx    context.Context
 		itemId string
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		wantErr bool
 	}{
 		// test 1
-		{name: "Remove item successfully", fields: fields{conn: conn}, args: args{ctx: context.Background(), itemId: "0000000001"}, wantErr: false},
+		{name: "Remove item successfully", args: args{ctx: context.Background(), itemId: "0000000001"}, wantErr: false},
 		// test 2
-		{name: "Remove item with error", fields: fields{conn: conn}, args: args{ctx: context.Background(), itemId: ""}, wantErr: true},
+		{name: "Remove item with error", args: args{ctx: context.Background(), itemId: ""}, wantErr: true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			r := &itemRepository{
-				conn: tt.fields.conn,
-			}
+			r := &itemRepository{}
 			if err := r.Remove(tt.args.ctx, tt.args.itemId); (err != nil) != tt.wantErr {
 				t.Errorf("itemRepository.Remove() error = %v, wantErr %v", err, tt.wantErr)
 			}
