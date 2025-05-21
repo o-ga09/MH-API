6162ffdc9afaaa9ef258d3012efbfec19f9e548d
o-ga09 <taitiabe1997@gmail.com>
2025-05-21 23:45:56 +0900

    🔧 fix: pkgを使用してslogのSeverityを修正し、logger.goからログレベル定義を削除しました

--- app/internal/presenter/middleware/logger.go
+++ app/internal/presenter/middleware/logger.go
@@ -14,15 +14,6 @@ import (
 	"log/slog"
 )
 
-// cloud logging の Log level 定義
-var (
-	Severitydefault = slog.Level(logging.Default)
-	SeverityInfo    = slog.Level(logging.Info)
-	SeverityWarn    = slog.Level(logging.Warning)
-	SeverityError   = slog.Level(logging.Error)
-	SeverityNotice  = slog.Level(logging.Notice)
-)
-
 // traceId , spanId 追加
 type traceHandler struct {
 	slog.Handler
