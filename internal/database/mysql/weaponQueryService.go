b1876443c9bc6ab2eaf4fa74ffd263d532a3f771
o-ga09 <taitiabe1997@gmail.com>
2025-05-23 21:08:26 +0900

    ✨ feat: 武器検索APIのFindWeaponsメソッドを改善し、パラメータの型を修正しました

--- internal/database/mysql/weaponQueryService.go
+++ internal/database/mysql/weaponQueryService.go
@@ -1,10 +1,11 @@
 package mysql
 
 import (
-	"context" // contextを追加
+	"context"
+	"mh-api/internal/domain/weapons"
+	weaponService "mh-api/internal/service/weapons"
 
-	"github.com/o-ga09/MH-API/internal/domain/weapons" // ドメイン層のweaponsパッケージをインポート
-	"gorm.io/gorm"                                // gormをインポート
+	"gorm.io/gorm"
 )
 
 // WeaponQueryService は武器データに関するクエリサービスです。
@@ -20,33 +21,21 @@ func NewWeaponQueryService(db *gorm.DB) *WeaponQueryService {
 // FindWeaponsParams は武器検索時のパラメータです。
 // Issue #83 で定義されたリクエストパラメータに対応します。
 type FindWeaponsParams struct {
-	Limit      *int    `json:"limit"`
-	Offset     *int    `json:"offset"`
-	Sort       *string `json:"sort"`
-	Order      *int    `json:"order"` // 0: Asc, 1: Desc (仮)
-	MonsterID  *string `json:"monster_id"`
-	Name       *string `json:"name"`
-	NameKana   *string `json:"name_kana"`
+	Limit     *int    `json:"limit"`
+	Offset    *int    `json:"offset"`
+	Sort      *string `json:"sort"`
+	Order     *int    `json:"order"` // 0: Asc, 1: Desc (仮)
+	MonsterID *string `json:"monster_id"`
+	Name      *string `json:"name"`
+	NameKana  *string `json:"name_kana"`
 	// 他にも必要なパラメータがあればここに追加
 }
 
-// FindWeapons は指定されたパラメータに基づいて武器のリストを検索します。
-// (注意: この初期実装ではダミーデータを返します)
-func (qs *WeaponQueryService) FindWeapons(ctx context.Context, params FindWeaponsParams) ([]*weapons.Weapon, int, error) {
-	// TODO: データベースから実際にデータを取得するロジックを後で実装
-	// TODO: params に基づいたフィルタリング、ソート、ページネーションを後で実装
+func (qs *WeaponQueryService) FindWeapons(ctx context.Context, params weaponService.SearchWeaponsParams) ([]*weapons.Weapon, int, error) {
+	dummyWeapons := []*weapons.Weapon{}
 
-	// ダミーの武器リスト (空または数件のダミーデータ)
-	dummyWeapons := []*weapons.Weapon{
-		// 例:
-		// weapons.NewWeapon("weapon_dummy_001", "初心者向け片手剣", "", "1", "100", "火", "緑", "0%", "とても使いやすい最初の武器。"),
-		// weapons.NewWeapon("weapon_dummy_002", "鉄刀【神楽】", "", "3", "150", "", "青", "10%", "熟練者向けの強力な太刀。"),
-	}
-
-	// ダミーの総件数
 	dummyTotalCount := len(dummyWeapons)
 
-	// limit と offset の簡単なダミー処理 (実際のデータ取得時に正しく実装する)
 	start := 0
 	if params.Offset != nil {
 		start = *params.Offset
@@ -57,14 +46,13 @@ func (qs *WeaponQueryService) FindWeapons(ctx context.Context, params FindWeapon
 			end = start + *params.Limit
 		}
 	}
-    
-    if start > dummyTotalCount {
-        return []*weapons.Weapon{}, dummyTotalCount, nil
-    }
-    if end > dummyTotalCount {
-        end = dummyTotalCount
-    }
 
+	if start > dummyTotalCount {
+		return []*weapons.Weapon{}, dummyTotalCount, nil
+	}
+	if end > dummyTotalCount {
+		end = dummyTotalCount
+	}
 
 	return dummyWeapons[start:end], dummyTotalCount, nil
 }
@@ -72,11 +60,5 @@ func (qs *WeaponQueryService) FindWeapons(ctx context.Context, params FindWeapon
 // FindWeaponByID は指定されたIDで武器を1件検索します。(これはIssueにはないが、あると便利かもしれないので枠だけ)
 // (注意: この初期実装ではダミーデータを返します)
 func (qs *WeaponQueryService) FindWeaponByID(ctx context.Context, weaponID string) (*weapons.Weapon, error) {
-    // TODO: データベースから実際にデータを取得するロジックを後で実装
-	
-    // ダミーデータ例
-    // if weaponID == "weapon_dummy_001" {
-    //     return weapons.NewWeapon("weapon_dummy_001", "初心者向け片手剣", "", "1", "100", "火", "緑", "0%", "とても使いやすい最初の武器。"), nil
-    // }
 	return nil, nil // 見つからない場合はnilを返す
 }
